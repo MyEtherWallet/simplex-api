248cc8c7347709c3c1e845837b995bd904b032af
SteveM <SteveM@myEtherWallet.com>
2019-06-19 18:51:45 -0700

    modify path to .env file

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   plugins: [
     new Dotenv({
-      path:path.resolve(__dirname, '../../')+'/.env'
+      path:path.resolve(__dirname, '../../')+'.env'
     })
   ],
   module: {
