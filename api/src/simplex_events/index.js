f9fd1c9b5982cc1683d08b9013236aa31dfaec11
SteveM <SteveM@myEtherWallet.com>
2019-08-23 15:18:09 -0700

    cron 1

--- api/src/simplex_events/index.js
+++ api/src/simplex_events/index.js
@@ -1,13 +1,48 @@
 import getEvents from './retrieveEvents'
+import cron from 'node-cron';
 
 import createLogger from 'logging'
 const logger = createLogger('simplex_events/index.js')
 
-getEvents()
-  .then(() => {
-    process.exit(0)
+//
+// getEvents()
+//   .then(() => {
+//     process.exit(0)
+//   })
+//   .catch(_error => {
+//     logger.error(_error)
+//     process.exit(1)
+//   })
+
+const cronTime = '* * * * *';
+
+// cron.schedule(cronTime, () => {
+//   getEvents()
+//     .then(() => {
+//       logger.info("Simplex Events Retrieved")
+//       // process.exit(0)
+//     })
+//     .catch(_error => {
+//       logger.error(_error)
+//       // process.exit(1)
+//     })
+// })
+
+
+const runCron = () => {
+  console.log('cronning')
+  return cron.schedule(cronTime, () => {
+    getEvents()
+      .then(() => {
+        logger.info("Simplex Events Retrieved")
+        // process.exit(0)
+      })
+      .catch(_error => {
+        logger.error(_error)
+        // process.exit(1)
+      })
   })
-  .catch(_error => {
-    logger.error(_error)
-    process.exit(1)
-  })
+}
+
+
+export default runCron
