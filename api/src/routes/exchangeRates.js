ee3cb5b528e4b285fe48d1df7949349951277a49
SteveM <SteveM@myEtherWallet.com>
2020-01-21 10:01:02 -0800

    calculate min and max by currency

--- api/src/routes/exchangeRates.js
+++ api/src/routes/exchangeRates.js
@@ -22,7 +22,7 @@ export default (app) => {
           }
         })
         response.success(res, {
-          cleanedRates,
+          rates: cleanedRates,
         });
       })
       .catch((error) => {
