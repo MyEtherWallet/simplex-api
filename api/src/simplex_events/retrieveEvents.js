b66ec7eb8c3b1cc813ac3c19ed7f63c9383878e2
SteveM <SteveM@myEtherWallet.com>
2020-02-18 13:41:57 -0800

    temp dev modifications

--- api/src/simplex_events/retrieveEvents.js
+++ api/src/simplex_events/retrieveEvents.js
@@ -24,6 +24,7 @@ connect().then(() => {
 
 let getEvents = () => {
   return new Promise((resolve, reject) => {
+    console.log('===================== getEvents ======================'); // todo remove dev item
     let options = {
       url: simplex.eventEP,
       headers: {
