287deef47801321f0f5a9ef6e3d01f58a376b305
SteveM <SteveM@myEtherWallet.com>
2020-04-28 15:05:21 -0700

    print out the info used to validate address

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -32,6 +32,7 @@ const validateMinMax = val => {
   return !(simplex.minFiat > +val || simplex.maxFiat < +val)
 }
 const validateAddress = val => {
+  console.log(val, cryptoSymbol); // todo remove dev item
   const maybeValid = simplex.validDigital.filter(cryptoSymbol => {
     return wav.validate(val, cryptoSymbol)
   })
